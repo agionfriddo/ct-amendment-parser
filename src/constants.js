c6486414782d4742ee7819a4990d7ec660287e56
Andrew Gionfriddo <example@example.com>
2025-03-05 21:14:09 -0500

    Update constants to new table names

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-const SENATE_AMENDMENTS_TABLE = "senate_amendments";
-const HOUSE_AMENDMENTS_TABLE = "house_amendments";
+const SENATE_AMENDMENTS_TABLE = "2025-senate-amendments";
+const HOUSE_AMENDMENTS_TABLE = "2025-house-amendments";
 const REGION = "us-east-1";
 
 module.exports = { SENATE_AMENDMENTS_TABLE, HOUSE_AMENDMENTS_TABLE, REGION };
