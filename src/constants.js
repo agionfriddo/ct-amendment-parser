310fd6357564774ed0412b455c43e39cb5a5cc69
Andrew Gionfriddo <example@example.com>
2025-03-06 08:39:07 -0500

    Rename some files

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 const SENATE_AMENDMENTS_TABLE = "2025-senate-amendments";
 const HOUSE_AMENDMENTS_TABLE = "2025-house-amendments";
+const BILLS_TABLE = "2025-bills";
 const REGION = "us-east-1";
 
 module.exports = { SENATE_AMENDMENTS_TABLE, HOUSE_AMENDMENTS_TABLE, REGION };
