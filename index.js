0a876f2173cc40948153bfeeb76b9b3ef3d57ee5
Andrew Gionfriddo <example@example.com>
2025-04-09 09:58:21 -0400

    Add email sending again to the app

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ async function main() {
 
     if (newSenateAmendments.length > 0) {
       console.log("Sending email for senate amendments");
-      // await sendEmail(newSenateAmendments, "senate");
+      await sendEmail(newSenateAmendments, "senate");
     }
     if (newHouseAmendments.length > 0) {
       console.log("Sending email for house amendments");
-      // await sendEmail(newHouseAmendments, "house");
+      await sendEmail(newHouseAmendments, "house");
     }
     return "Success!";
   } catch (e) {
@@ -34,4 +34,4 @@ async function main() {
 
 exports.handler = main;
 
-main();
+// main();
